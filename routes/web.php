d84af0e409929b26916941c6ac905a3a163ff7e3
haruki20000416 <h.sato.0416@gmail.com>
2022-01-27 16:25:22 +0900

    [add]初期データ表示のためのルーティング

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,6 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'PostController@index');
+Route::get('/picher', 'PostController@show');
+Route::get('/information','PostController@view');
