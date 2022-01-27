69e8417e24b9e56daaf1dd13de26e6a3723b3c73
haruki20000416 <h.sato.0416@gmail.com>
2022-01-27 16:12:01 +0900

    [add]テーブルの作成

---
+++ database/migrations/2022_01_23_143908_create_pichers_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePichersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pichers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('uniname',50);
+            $table->string('pichername',50);
+            $table->string('handedness',50);
+            $table->integer('quick');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pichers');
+    }
+}
---
+++ database/migrations/2022_01_24_135437_create_games_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateGamesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('games', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('batter',50);
+            $table->integer('inning');
+            $table->integer('outcount');
+            $table->integer('count');
+            $table->string('ballspecies',50);
+            $table->integer('speed');
+            $table->string('result',50);
+            $table->integer('picher_id')->unsigned();
+            $table->integer('information_id')->unsigned();
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('games');
+    }
+}
