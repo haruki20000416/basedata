467127866b6db40e56762cf57b0de5620da83c31
haruki20000416 <h.sato.0416@gmail.com>
2022-02-09 18:20:45 +0900

    [update]データ表示の変更

--- app/Http/Controllers/DataController.php
+++ app/Http/Controllers/DataController.php
@@ -50,32 +50,213 @@ class DataController extends Controller
         $ballLs = $ballL2->countBy();
     
         
-        //ストライク率
-        $strike=Game::with('picher')
+        //ストライク率 
+         $strike=Game::with('picher')
                ->where('picher_id',$picher_id)
                ->where('course','>',0)
                ->get();
         $strike2 = $strike->pluck('ballspecies');
         $strikes = $strike2->countBy();
-    
+
         
-        //カウント別投球割合
-    
-                
-                
-                
-        //コース内訳
-        $course= $strike=Game::with('picher')
+        //ストレートの総数
+        $strikesta=Game::with('picher')
                ->where('picher_id',$picher_id)
-               ->where('course','>',0)
-               ->select('ballspecies','course')
-               ->get();
+               ->where('ballspecies','ストレート')
+               ->count();
+        //ツーシームの総数
+         $striketwa=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','ツーシーム')
+               ->count();
+        //縦スライダー総数
+        $striketsraa=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','縦スライダー')
+               ->count();
+       //横スライダー総数
+       $strikeysraa=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','横スライダー')
+               ->count();
+        //カットボール総数
+        $strikecuta=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','カットボール')
+               ->count();
+        //カーブ総数
+        $strikecura=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','カーブ')
+               ->count();
+        //シュート総数
+        $strikechua=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','シュート')
+               ->count();
+        //シンカー総数
+        $strikesina=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','シンカー')
+               ->count();
+        //チェンジアップ総数
+        $strikechea=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','チェンジアップ')
+               ->count();
+        //スプリット総数
+        $strikespa=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','スプリット')
+               ->count();
+        //フォーク総数
+        $strikefora=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('ballspecies','フォーク')
+               ->count();
                
+               
+        //カウント別投球割合
+           
+         //0-0
+            $count00=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','01')
+               ->get();
+            $count00C = $count00->count();  
+            $count00s = $count00->pluck('ballspecies');
+            $count00CB = $count00s->countBy(); 
+    
+         //0-1
+            $count01=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','01')
+               ->get();
+            $count01C = $count01->count();  
+            $count01s = $count01->pluck('ballspecies');
+            $count01CB = $count01s->countBy();  
+         //0-2
+            $count02=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','02')
+               ->get();
+            $count02C = $count02->count();  
+            $count02s = $count02->pluck('ballspecies');
+            $count02CB = $count02s->countBy();
+         //1-0    
+            $count10=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','10')
+               ->get();
+            $count10C = $count10->count();  
+            $count10s = $count10->pluck('ballspecies');
+            $count10CB = $count10s->countBy();    
+                
+         //1-1
+            $count11=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','11')
+               ->get();
+            $count11C = $count11->count();  
+            $count11s = $count11->pluck('ballspecies');
+            $count11CB = $count11s->countBy();
+            
+         //1-2
+           $count12=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','12')
+               ->get();
+            $count12C = $count12->count();  
+            $count12s = $count12->pluck('ballspecies');
+            $count12CB = $count12s->countBy();
+            
+         //2-0  
+           $count20=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','20')
+               ->get();
+            $count20C = $count20->count();  
+            $count20s = $count20->pluck('ballspecies');
+            $count20CB = $count20s->countBy();
+          
+         //2-1
+          $count21=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','21')
+               ->get();
+            $count21C = $count21->count();  
+            $count21s = $count21->pluck('ballspecies');
+            $count21CB = $count21s->countBy();
+         
+         //2-2
+           $count22=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','22')
+               ->get();
+            $count22C = $count22->count();  
+            $count22s = $count22->pluck('ballspecies');
+            $count22CB = $count22s->countBy();
+         //3-0
+          $count30=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','30')
+               ->get();
+            $count30C = $count30->count();  
+            $count30s = $count30->pluck('ballspecies');
+            $count30CB = $count30s->countBy();
+            
+         //3-1
+           $count31=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','31')
+               ->get();
+            $count31C = $count31->count();  
+            $count31s = $count31->pluck('ballspecies');
+            $count31CB = $count31s->countBy();
+            
+         //3-2
+           $count32=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('count','32')
+               ->get();
+            $count32C = $count32->count();  
+            $count32s = $count32->pluck('ballspecies');
+            $count32CB = $count32s->countBy();
+        
+        if($count00C == null){
+            
+            $count00C=0 and $countCB=0;
+            
+        }
+        
+        
+        //コース内訳 右
+        $courseR=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('batter','右')
+               ->where('course','>',0)
+               ->get();
+        $courseR2 = $courseR->pluck('course');
+        $courseRs = $courseR2->countBy();
+        
+        //コース内訳　左
+        $courseL=Game::with('picher')
+               ->where('picher_id',$picher_id)
+               ->where('batter','左')
+               ->where('course','>',0)
+               ->get();
+        $courseL2 = $courseL->pluck('course');
+        $courseLs = $courseL2->countBy();
         
         
         
         
         return view('datashow',compact('name','data','speedaves','count','ballspecies2s',
-                                        'strikes','picher_id','ballrs','countR','ballLs','countL'));
+                                        'picher_id','ballrs','countR','ballLs','countL','strikesta','strikes',
+                                         'strikeysraa', 'strikeysraa','strikecuta','strikecura','strikechua','strikesina','strikechea', 'strikespa'
+                                         ,'strikefora','striketwa','count00C','count00CB','count01C','count01CB','count02C','count02CB',
+                                         'count10C','count10CB','count11C','count11CB','count12C','count12CB','count20C','count20CB',
+                                        'count21C','count21CB','count22C','count22CB','count30C','count30CB','count31C','count31CB','count32C',
+                                        'count32CB'));
     }
 }
--- resources/views/datashow.blade.php
+++ resources/views/datashow.blade.php
@@ -3,41 +3,167 @@
 @section('title','データ詳細画面')
 
 @section('content')
-        <div class="name">
-            <h2>{{$name->picher->uniname}}  {{$name->picher->pichername}}投手</h2>
-        </div>
-          <h2>平均球速</h2>   
+<div class="name">
+    
+    
+    <h1>{{$name->picher->uniname}}  {{$name->picher->pichername}}投手</h1>
+    
+</div>
+
+<div class="speed">      
+
+          <h2>平均球速</h2>  
         @foreach ($speedaves as $speedave)
-        <div class="球種">
-            
-            <p>{{$speedave->ballspecies}}→<?php echo ceil($speedave->speed);?>km</p>
+        
+            <p>{{$speedave->ballspecies}}→{{ceil($speedave->speed)}}km</p>
         
         @endforeach
+</div>
+
+<div class="rate">
+    
         <h2>投球割合</h2>
         @foreach($ballspecies2s as $key =>$value)
-            <p>{{$key}}→<?php echo ceil($value/$count*100);?>％</p>
+            <p>{{$key}}→{{ceil($value/$count*100)}}％</p>
             
         @endforeach
-        
+</div>
+    // ここの大きさ調整
+    <div style="position: center; height:100; width:150">
+        <canvas id="myChart"> ></canvas>
+
+        <script src="{{ mix('js/app.js') }}"></script>
+    
+    </div>
+
+<div class="rateR">
         <h2>投球割合　右</h2>
         @foreach($ballrs as $key => $value)
-           <p>{{$key}}-><?php echo ceil($value/$countR*100);?>％</p>
+           <p>{{$key}}->{{ceil($value/$countR*100)}}％</p>
             
         @endforeach
-        
+</div>
+
+<div class="rateL">
          <h2>投球割合　左</h2>
         @foreach($ballLs as $key => $value)
-           <p>{{$key}}-><?php echo ceil($value/$count100);?>％</p>
+           <p>{{$key}}->{{ceil($value/$countL*100)}}％</p>
             
         @endforeach
-        
+</div>  
+
+<div class="strike">
         <h2>ストライク率</h2>
         @foreach($strikes as $key =>$value)
-        //÷母数が全体になってしまっている
-            <p>{{$key}}→<?php echo ceil($value/$count*100);?>％</p>
+           @if($key == "ストレート") 
+            <p>{{$key}}→{{ceil($value/$strikesta*100)}}％</p>
+           @elseif($key == "ツーシーム―")
+            <p>{{$key}}→{{ceil($value/$striketwa*100)}}％</p>
+           @elseif ($key == "縦スライダー")
+            <p>{{$key}}→{{ceil($value/$striketsraa*100)}}％</p>
             
+           @elseif ($key == "横スライダー")
+           <P>{{$key}}→{{ceil($value/$strikeysraa*100)}}％</P>
+           
+           @elseif ($key =="カットボール")
+           <p>{{$key}}→{{ceil($value/$strikecuta*100)}}％</p>
+           
+           @elseif($key == "カーブ")
+           <p>{{$key}}→{{ceil($value/$strikecura*100)}}％</p>
+           
+           @elseif ($key == "シュート")
+           <p>{{$key}}→{{ceil($value/$strikechua*100)}}％</p>
+           
+           @elseif ($key == "シンカー")
+           <p>{{$key}}→{{ceil($value/$strikesina*100)}}％</p>
+           
+           @elseif ($key == "チェンジアップ")
+           <p>{{$key}}→{{ceil($value/$strikechea*100)}}％</p>
+           
+           @elseif ($key == "スプリット")
+           <p>{{$key}}→{{ceil($value/$strikespa*100)}}％</p>
+           
+           @elseif ($key == "フォーク")
+           <p>{{$key}}→{{ceil($value/$strikefora*100)}}％</p>
+           @endif
+           
         @endforeach
+</div>       
         
-        
+<div class="count">
+    <h2>カウント別球種</h2>
+    <div class="00">
+        <h3>0B0S</h3>
+        @foreach($count00CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count00C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="01">
+        <h3>0B1S</h3>
+        @foreach($count01CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count01C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="02">
+        <h3>0B2S</h3>
+        @foreach($count02CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count02C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="10">
+        <h3>1B0S</h3>
+        @foreach($count10CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count10C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="11">
+        <h3>1B1S</h3>
+        @foreach($count11CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count11C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="12">
+        <h3>1B2S</h3>
+        @foreach($count12CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count12C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="20">
+        <h3>2B0S</h3>
+        @foreach($count20CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count20C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="21">
+        <h3>2B1S</h3>
+        @foreach($count21CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count21C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="22">
+        <h3>2B2S</h3>
+        @foreach($count22CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count22C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="30">
+        <h3>3B0S</h3>
+        @foreach($count30CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count30C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="31">
+        <h3>3B1S</h3>
+        @foreach($count31CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count31C*100)}}％</P>
+        @endforeach
+    </div>
+    <div class="32">
+        <h3>3B2S</h3>
+        @foreach($count32CB as $key =>$value)
+           <P>{{$key}}→{{ceil($value/$count32C*100)}}％</P>
+        @endforeach
+    </div>
+</div>        
 @endsection
     
