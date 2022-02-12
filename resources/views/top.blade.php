e42904f10104f3abfeba9ba1cdadd33779d7d443
haruki20000416 <h.sato.0416@gmail.com>
2022-02-12 17:48:13 +0900

    [update] ファイル名の変更

---
+++ resources/views/top.blade.php
@@ -0,0 +1,115 @@
+@extends('layout')
+
+@section('title','トップ画面')
+
+@section('content')
+
+
+
+ <div class="text-center">
+    <div class="box1">
+        <h1 class="display-4">
+          "最高の結果は、最高の準備から"
+        </h1>
+    </div>   
+ </div>    
+
+<div class="mb-5">
+    <img src="img/fukudai3.jpg" class="img-fluid" alt="max-width: 50%; , height: auto%;">
+</div>
+    
+<div class='box3'>
+    <a id="data"></a>
+    <h1 class="text-center">データ入力</h1>
+
+    
+</div>
+    
+<div class="d-grid gap-2 col-6 mx-auto">
+
+    <a href="/informations"  class="btn btn-outline-info">
+        試合情報入力へ進む
+    </a>
+    <a href='/picher' class="btn btn-outline-secondary">
+            投手情報入力へ進む
+    </a>
+</div>
+
+<div class="box2"></div> 
+<a id="show"></a>
+<div class="mb-5">
+　　<h2 class="text-center">投手データ一覧</h2>
+</div>　　
+　　
+<div class="container">
+  <div class="row">
+    <div class="col">
+     <h3 class="fukudai">福島大学</h3>  
+    @foreach ($picherfukus as $picherfuku)
+        <p class='text-dark'>
+            
+            <a href="/pichers/view/{{$picherfuku->id}}">{{$picherfuku->pichername}}投手</a>
+            
+        </p>
+    @endforeach
+    </div>
+    <div class="col">
+      <h3 class="kokusai">東日本国際大学</h3>  
+    @foreach ($picherkokus as $picherkoku)
+        <p class=''>
+            
+            <a href="/pichers/view/{{$picherkoku->id}}">{{$picherkoku->pichername}}投手</a>
+            
+        </p>
+    @endforeach
+    </div>
+    <div class="col">
+      <h3 class="koueki">東北公益文科大学</h3>  
+    @foreach ($picherkous as $picherkou)
+        <p class=''>
+            
+            <a href="/pichers/view/{{$picherkou->id}}">{{$picherkou->pichername}}投手</a>
+            
+        </p>
+    @endforeach
+    </div>
+  </div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col">
+     <h3 class="nichiko">日本大学工学部</h3>  
+    @foreach ($pichernichis as $pichernichi)
+        <p class=''>
+            
+            <a href="/pichers/view/{{$pichernichi->id}}">{{$pichernichi->pichername}}投手</a>
+            
+        </p>
+    @endforeach
+    </div>
+    <div class="col">
+      <h3 class="ishi">石巻専修大学</h3>  
+    @foreach ($picherishis as $picherishi)
+        <p class=''>
+            
+            <a href="/pichers/view/{{$picherishi->id}}">{{$picherishi->pichername}}投手</a>
+            
+        </p>
+    @endforeach
+    </div>
+    <div class="col">
+      <h3 class="yama">山形大学</h3>  
+    @foreach ($picheryamas as $picheryama)
+        <p class=''>
+            
+            <a href="/pichers/view/{{$picheryama->id}}">{{$picheryama->pichername}}投手</a>
+            
+        </p>
+    @endforeach
+    </div>
+  </div>
+</div>
+        
+  
+
+@endsection
