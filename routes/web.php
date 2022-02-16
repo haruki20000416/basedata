03f3c5b1f209bee3dc67f4347d256e3d567cbae7
haruki20000416 <h.sato.0416@gmail.com>
2022-02-16 09:53:10 +0900

    [updata]line api

--- routes/web.php
+++ routes/web.php
@@ -12,12 +12,21 @@
 */
 
 Route::get('/', 'PostController@index');
-Route::get('/picher', 'PostController@show');
 Route::get('/informations','PostController@create');
 Route::post('/informations', 'PostController@store');
 Route::get('/informations/{information}', 'PostController@view');
-Route::get('/informations/{information}/games','PostController@gamecreate');
-Route::post('/informations/{information}/games', 'PostController@gamestore');
+Route::get('/games','PostController@gamecreate');
+Route::post('/games', 'PostController@gamestore');
+Route::get('/game/{game}','PostController@gamereturn'); //多分いらない
 Route::post('/pichers','PostController@picherstore');
 Route::get('/picher','PostController@pichercreat');
+Route::get('/pichers/view/{picher}','DataController@dataview');
 Route::get('/pichers/{picher}','PostController@picherview');
+Route::get('/pichers/{picher}/edit', 'PostController@edit');
+Route::put('/pichers/{picher}', 'PostController@update');
+Route::get('/games/id','PostController@show');
+// LINE メッセージ受信
+Route::post('/line/webhook', 'LineMessengerController@webhook')->name('line.webhook');
+ 
+// LINE メッセージ送信用
+Route::get('/line/message', 'LineMessengerController@message');
